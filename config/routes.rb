64d9133191308ae369ecc7a6642c1946b5183857
yusuke-takada00 <yu.neiho@gmail.com>
2020-05-01 15:25:42 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#top'
   get "/signup" => "users#new"
+  
+  
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+  
   resources :users
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
