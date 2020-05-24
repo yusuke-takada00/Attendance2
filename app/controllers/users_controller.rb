b177b505771477009fbfe540a1a4dcff3143813a
yusuke-takada00 <yu.neiho@gmail.com>
2020-05-24 03:48:27 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:destroy, :show, :edit, :update]
-  before_action :admin_user, only: [:destroy]
-  before_action :logged_in_user, only: [:destory, :index, :show, :edit, :update]
+  before_action :set_user, only: [:destroy, :show, :edit, :update, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:destory, :index, :show, :edit, :update, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   
   def index                                                                                                                                                                                                                                                                                                                                                                                                            
@@ -44,10 +44,26 @@ class UsersController < ApplicationController
     flash[:success] = "#{@user.name}のデータを削除しました"
     redirect_to users_url
   end
+  
+  def edit_basic_info
+  end
+  
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] ="#{@user.name}の基本情報を更新しました"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
+  end
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :department)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
     
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
   root 'static_pages#top'
-  get "/signup" => "users#new"
-  
-  
-  get "/login" => "sessions#new"
-  post "/login" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
-  
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
